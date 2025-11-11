f25804b8a4e67e5d07d8667b4d1d03dfc1d62671
ykm99999 <119298674+ykm99999@users.noreply.github.com>
2025-11-11 23:16:03 +0800

    Update upload_to_api.py

--- upload_to_api.py
+++ upload_to_api.py
@@ -5,9 +5,9 @@ def read_ips():
         return [line.strip() for line in f if line.strip()]
 
 def upload(ips):
-    url = "https://999.f.7.6.b.0.d.0.0.1.0.a.2.ip6.arpa/api/upload"    
+    url = "https://999.f.7.6.b.0.d.0.0.1.0.a.2.ip6.arpa/api/upload"      
     payload = {
-        "uuid": "7f4ca7a1-3dd6-47c5-a81f-343746e4d233",    
+        "uuid": "7f4ca7a1-3dd6-47c5-a81f-343746e4d233",      
         "ips": ips
     }
     r = requests.post(url, json=payload)
